515ec27ce757aa5a07735c9ce942ab993feb5149
ZNW_Mac <244407031@qq.com>
2019-11-12 21:53:31 +0800

    添加语法分析代码

---
+++ Cifafxi/cmake-build-debug/CMakeFiles/Cifafxi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cifafxi.dir/main.cpp.o"
+  "Cifafxi.pdb"
+  "Cifafxi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cifafxi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
